475bb4d81b6325f8ad1b83fdc38349b2c4bdc258
Evan Thomas <evan@evanjt.com>
2026-01-16 14:53:41 +0100

    Remove destination flag in detox, add missing testid

--- .detoxrc.js
+++ .detoxrc.js
@@ -15,14 +15,14 @@ module.exports = {
       binaryPath:
         'ios/build/Build/Products/Debug-iphonesimulator/Veloq.app',
       build:
-        "xcodebuild -workspace ios/Veloq.xcworkspace -scheme Veloq -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build -destination 'generic/platform=iOS Simulator'",
+        'xcodebuild -workspace ios/Veloq.xcworkspace -scheme Veloq -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build',
     },
     'ios.release': {
       type: 'ios.app',
       binaryPath:
         'ios/build/Build/Products/Release-iphonesimulator/Veloq.app',
       build:
-        "xcodebuild -workspace ios/Veloq.xcworkspace -scheme Veloq -configuration Release -sdk iphonesimulator -derivedDataPath ios/build -destination 'generic/platform=iOS Simulator'",
+        'xcodebuild -workspace ios/Veloq.xcworkspace -scheme Veloq -configuration Release -sdk iphonesimulator -derivedDataPath ios/build',
     },
     'android.debug': {
       type: 'android.apk',
