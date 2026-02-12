42723441db401406d5789fe106f1824206543c18
Evan Thomas <evan@evanjt.com>
2026-02-12 23:20:21 +0100

    remove check on CH locale in release

--- scripts/store-metadata.ts
+++ scripts/store-metadata.ts
@@ -40,6 +40,7 @@ const LOCALE_MAPPINGS: LocaleMapping[] = [
   { app: 'de-DE', android: 'de-DE', ios: 'de-DE' },
   // Swiss German variants: de-CH is NOT a valid Play Store locale (causes API errors)
   // Use de-DE for Android, de-CH for iOS only
+  { app: 'de-CH', android: 'de-DE', ios: 'de-CH' },
   { app: 'de-CHB', android: 'de-DE', ios: 'de-CH' },
   { app: 'de-CHZ', android: 'de-DE', ios: 'de-CH' },
 
