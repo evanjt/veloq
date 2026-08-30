f4dcb1b39fe5d3eb9ddfe2d21be6d5c9a9ff3f8c
Evan Thomas <evan@evanjt.com>
2026-08-30 09:59:21 +0200

    Delete InsightLine and its summary card slot, disabled by product direction

--- src/features/home/components/index.ts
+++ src/features/home/components/index.ts
@@ -2,6 +2,5 @@ export { SummaryCard } from './SummaryCard';
 export type { SummaryCardProps } from './SummaryCard';
 export { SummaryCardSparkline } from './SummaryCardSparkline';
 export { SummaryCardHRVSparkline } from './SummaryCardHRVSparkline';
-export { InsightLine } from './InsightLine';
 export { NotificationOptInCard } from './NotificationOptInCard';
 export { SupportCard } from './SupportCard';
