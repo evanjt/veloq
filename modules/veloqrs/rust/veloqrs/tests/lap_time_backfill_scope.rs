071540f31dcb5dacd7901b60fcf16bc215151c04
Evan Thomas <evan@evanjt.com>
2026-09-05 19:35:13 +0200

    Expect the half-open end in the lap time backfill scope test

--- modules/veloqrs/rust/veloqrs/tests/lap_time_backfill_scope.rs
+++ modules/veloqrs/rust/veloqrs/tests/lap_time_backfill_scope.rs
@@ -97,7 +97,7 @@ fn a_portion_whose_stream_has_landed_is_filled() {
     let times: Vec<u32> = (0..40).collect();
     engine.set_time_streams_flat(&["streamed".into()], &times, &[0]);
 
-    assert_eq!(lap_time(&db, "streamed"), Some(20.0));
+    assert_eq!(lap_time(&db, "streamed"), Some(19.0));
     assert_eq!(lap_time(&db, "bare"), None);
 }
 
@@ -115,7 +115,7 @@ fn only_the_streamed_portion_is_examined_once_a_stream_lands() {
     .expect("store the stream");
 
     assert_eq!(engine.backfill_section_performance_cache(), 1);
-    assert_eq!(lap_time(&db, "streamed"), Some(20.0));
+    assert_eq!(lap_time(&db, "streamed"), Some(19.0));
     assert_eq!(lap_time(&db, "bare"), None);
 }
 
